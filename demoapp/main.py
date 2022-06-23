2ee793a19143fdb3fd56ddb4abefff164e061893
Rozaki0318 <r.ozaki0318@gmail.com>
2022-06-23 13:47:13 +0900

    refactoring main.oy

--- demoapp/main.py
+++ demoapp/main.py
@@ -1,10 +1,29 @@
-class Roboko(object):
-  def __init__(self, name):
-    self.name = name
-    print('{} has been created!'.format(self.name))
+import csv
 
-  def say_hello(self):
-    print('Hello friends! How is it going?')
+class Roboko(object):
+    def __init__(self, name):
+        self.name = name
+        print('{} has been created!'.format(self.name))
+        
+    def ask_name(self):
+        word = "Hello! May I ask your name?\n"
+        print('=' * len(word) + '\n' + word + '=' * len(word) + '\n')
+        ans = input()
+        return ans
+    
+    def ask_restaurant(self, username):
+        print('===========================\n{}-san, Which restaurant do you like?\n==========================='.format(username))
+        return input()
+
+
+
+ #   with open('restaurant.csv', 'w') as csv_file:
+ #     fieldnames = ['Name','Count']
+ #     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+ #    writer.writeheader()
+ #     writer.writerow({'Name': ans, 'Count': '1'})
 
 roboko = Roboko('Hanako')
-roboko.say_hello()
+username = roboko.ask_name()
+restaurant = roboko.ask_restaurant(username)
+print(username, restaurant)
