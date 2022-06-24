508dfbdc7c0e85482abe9df5387e6c91f51ee8de
Rozaki0318 <r.ozaki0318@gmail.com>
2022-06-24 12:34:04 +0900

    I did it! but ugly code

--- demoapp/main.py
+++ demoapp/main.py
@@ -1,84 +1,116 @@
 import csv
 from pathlib import Path
-import logging
-import re
-
-formatter = '%(asctime)s:%(levelname)s:%(message)s'
-logging.basicConfig(filename='demoapp.log', level=logging.DEBUG, format=formatter)
 
 class Roboko(object):
     def __init__(self, name):
         self.name = name
-        self.csvfile = 'restaurant.csv'
-        logging.debug('{} has been created!'.format(self.name))
-        path = Path(self.csvfile)
+        self.list = []
+        path = Path('./restaurant_list.csv')
         if not path.is_file():
-            logging.debug('restaurant.csv not found, will be created.')
-            with open('./restaurant.csv','w') as csv_file:
-                fieldnames = ['Name', 'Count']
+            with open('restaurant_list.csv', 'w') as csv_file:
+                fieldnames = ['Name','Count']
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writeheader()
         else:
-            logging.debug('restaurant.csv already exists. Nothing to do.')
-        
-    def ask_name(self):
-        word = "Hello! May I ask your name?\n"
-        print('=' * len(word) + '\n' + word + '=' * len(word) + '\n')
-        ans = input()
-        if not ans:
-            return self.ask_name()
-        return ans
-    
-    def ask_restaurant(self, username, recommend_restaurant):
-        if not recommend_restaurant:
-            word = "{}-san, Which restaurant do you like?\n".format(username)
-            print('=' * len(word) + '\n' + word + '=' * len(word) + '\n')
-            ans = input()
-            if not ans:
-                return self.ask_restaurant(username)
-            return ans
+            self.list = self.file_read()
+
+    def say_hello(self):
+        print('Hello! I\'m {}. May I ask your name?'.format(self.name))
+        username = input().capitalize()
+        if username == '':
+            return self.say_hello()
         else:
-            word = "{}-san, My recommend is {}. Do you like that?\n".format(username, recommend_restaurant)
-            print('=' * len(word) + '\n' + word + '=' * len(word) + '\n')
-            yorn = input()
-            if not yorn:
-                return self.ask_restaurant(username, recommend_restaurant)
-            if yorn == "yes":
-                return recommend_restaurant
+            return username
+
+    def recommend(self, username):
+        list = self.list
+        if len(list) == 0:
+            print('{}-san, which restaurant do you like?'.format(username))
+            user_recommend = input()
+            if user_recommend == '':
+                return self.recommend(username)
+            list = [[user_recommend, 1]]
+            self.file_write(list)
+        elif len(list) == 1:
+            print('{}-san, my recommendation is {}. Do you like that? yes or no'.format(username, list[0][0]))
+            ans = input().capitalize()
+            if ans == 'Yes':
+                list[0][1] = list[0][1] + 1
+                self.file_write(list)
             else:
-                return self.ask_restaurant(username, "")
-    
+                print('{}-san, which restaurant do you like?'.format(username))
+                user_recommend = input()
+                if user_recommend == '':
+                    return self.recommend(username)
+                else:
+                    for f in list:
+                        flag = 0
+                        if user_recommend == f[0]:
+                            f[1] = f[1] + 1
+                            flag = 1
+                    if flag == 0:
+                        list.append([user_recommend, 1])
+                self.file_write(list)
+        else:
+            max_value = 0
+            for i in 0,len(list)-2:
+                print(i)
+                if list[i][1] > list[i+1][1]:
+                    max_value = list[i][1]
+                elif list[i][1] < list[i+1][1]:
+                    max_value = list[i+1][1]
+                elif list[i][1] == list[i+1][1]:
+                    max_value = list[i][1]
+            for f in list:
+                if max_value == f[1]:
+                    recommend_restaurant = f[0]
+            print('{}-san, my recommendation is {}. Do you like that? yes or no'.format(username, recommend_restaurant))
+            ans = input().capitalize()
+            if ans == 'Yes':
+                for f in list:
+                  if recommend_restaurant == f[0]:
+                    f[1] = f[1] + 1
+                self.file_write(list)
+            else:
+                print('{}-san, which restaurant do you like?'.format(username))
+                user_recommend = input()
+                if user_recommend == '':
+                    return self.recommend(username)
+                else:
+                    for f in list:
+                        flag = 0
+                        if user_recommend == f[0]:
+                            f[1] = f[1] + 1
+                            flag = 1
+                    if flag == 0:
+                        list.append([user_recommend, 1])
+                self.file_write(list)
+        
+
     def file_read(self):
-        logging.debug('process is going into file_read')
-        with open('./restaurant.csv', 'r') as csv_file:
+        with open('restaurant_list.csv', 'r') as csv_file:
             reader = csv.DictReader(csv_file)
-            restaurant_list = []
+            list = []
             for row in reader:
-                restaurant_list.append([row['Name'], row['Count']])
-            if len(restaurant_list) > 1:
-                for i in len(restaurant_list)-1,-1,1:
-                    if restaurant_list[i][1] > restaurant_list[i-1][1]: 
-                        recommend_restaurant = restaurant_list[i][0]
-                    elif restaurant_list[i][1] < restaurant_list[i-1][1]:
-                        recommend_restaurant = restaurant_list[i-1][0]
-                    else:
-                        recommend_restaurant = restaurant_list[i][0]
-            elif len(restaurant_list) == 1:
-                recommend_restaurant = restaurant_list[0][0]
-            else:
-                recommend_restaurant = ""
-            logging.debug('{} is returned from file_read, as recommendation'.format(recommend_restaurant))
-            return recommend_restaurant
-    
-    def file_append(self, restaurant):
-        with open('./restaurant.csv','a') as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerow([restaurant, 1])
+                list.append([row['Name'], int(row['Count'])])
+        return list
+
+
+    def file_write(self, list):
+        with open('./restaurant_list.csv', 'r+') as csv_file:
+            csv_file.truncate(0)
+        with open('./restaurant_list.csv', 'w') as csv_file:
+            fieldnames = ['Name','Count']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            for row in list:
+                writer.writerow({'Name': row[0], 'Count': row[1]})
+
+    def say_goodbye(self):
+        print('Thanks! Good bye!')
 
 roboko = Roboko('Hanako')
-recommend_restaurant = roboko.file_read()
-print(recommend_restaurant)
-username = roboko.ask_name()
-restaurant = roboko.ask_restaurant(username, recommend_restaurant)
-print(username, restaurant)
-roboko.file_append(restaurant)
+print(roboko.list)
+username = roboko.say_hello()
+roboko.recommend(username)
+roboko.say_goodbye()
