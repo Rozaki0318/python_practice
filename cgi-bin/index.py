d35807e12ba656ed6c9aecc625d4791d3ced0f25
Rozaki0318 <r.ozaki0318@gmail.com>
2022-07-06 11:33:35 +0900

    learn http server with cgi

---
+++ cgi-bin/index.py
@@ -0,0 +1,19 @@
+#!/Users/ryoyaozaki/projects/python_practice/pypra/pypra/bin/python
+# -*- coding: utf-8 -*-
+
+print ("""
+<!doctype html>
+
+<html lang="es">
+
+<head>
+<meta charset="utf-8"/>
+</head>
+
+<body>
+
+<header>
+<h1>Python is running.</h1>
+</header>
+""")
+
--- httpserver.py
+++ httpserver.py
@@ -1,6 +1,9 @@
 import http.server
-import socketserver
 
-with socketserver.TCPServer(('127.0.0.1', 8000),
-                            http.server.SimpleHTTPRequestHandler) as httpd:
+
+Handler = http.server.CGIHTTPRequestHandler
+# socketserverでCGIHTTPRequestHandler使うとエラーでcgi実行に失敗した。
+
+with http.server.HTTPServer(('127.0.0.1', 8001), Handler) as httpd:
+    print("serving at port", 8001)
     httpd.serve_forever()
