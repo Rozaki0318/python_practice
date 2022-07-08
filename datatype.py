b983998e51434f4c4881e4e8a408a3f920dc293b
Rozaki0318 <r.ozaki0318@gmail.com>
2022-07-09 08:09:46 +0900

    learn dict w/ for

--- datatype.py
+++ datatype.py
@@ -5,3 +5,11 @@ print(d['z'])
 print(d2)
 d.update(d2)
 print(d)
+
+for v in d:
+    print('value:{}'.format(v))
+# Keyがprintされる
+
+for k, v in d.items():
+    print('key:{}, value:{}'.format(k, v))
+# Dict型が持つitemsメソッドを使うと、KeyとValueがprintされる
