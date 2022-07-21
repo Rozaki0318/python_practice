715e4f7c18e30412e8c67ca34636da9bd2c56a73
Rozaki0318 <r.ozaki0318@gmail.com>
2022-07-21 11:21:43 +0900

    learn bash script

--- gather-info.sh
+++ gather-info.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
+line="------------------------------"
 echo "Starting at: $(date)"
-echo
+echo $line
 
 echo "UPTIME"
 uptime
-echo
-
+echo $line
 echo "FREE"
 free
-echo
-
+echo $line
 echo "WHO"
 who
-echo
-
+echo $line
 echo "Finishing at: $(date)"
