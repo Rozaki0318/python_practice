e8c8bc4743ef6064c4685ba0ed9942af10a6070a
Rozaki0318 <r.ozaki0318@gmail.com>
2022-08-12 05:29:40 +0900

    add process_image

---
+++ pypra/process_image.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from PIL import Image
+import os
+
+path = './images/'
+save_path = '/opt/icons/'
+
+filelist = [s for s in os.listdir(path)]
+print(filelist)
+
+for image in filelist:
+    im = Image.open(path + image)
+    print(im.format, im.size, im.mode)
+    resized_im = im.rotate(90).resize((128, 128)).convert('RGB').save(save_path + image, 'JPEG')
