35d3abc6925751e2ed9ed2744af1bc70d4ec8cb7
Rozaki0318 <r.ozaki0318@gmail.com>
2022-08-12 06:09:23 +0900

    add process_image

--- pypra/process_image.py
+++ pypra/process_image.py
@@ -10,6 +10,9 @@ filelist = [s for s in os.listdir(path)]
 print(filelist)
 
 for image in filelist:
-    im = Image.open(path + image)
-    print(im.format, im.size, im.mode)
-    resized_im = im.rotate(90).resize((128, 128)).convert('RGB').save(save_path + image, 'JPEG')
+    if image == '.DS_Store':
+        continue
+    else:
+        im = Image.open(path + image)
+        print(im.format, im.size, im.mode)
+        resized_im = im.rotate(90).resize((128, 128)).convert('RGB').save(save_path + image, 'JPEG')
